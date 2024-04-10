9a9632af7eebcd3b87abcfbc2218f4ea65ccf21e
itgeeksinc <support@itgeeks.com>
2024-04-10 15:50:07 +0530

    Web dashboard and other foldera add

---
+++ lib/utils/color_const.dart
@@ -0,0 +1,37 @@
+
+// to change colors code
+import 'dart:ui';
+
+class ColorConsts {
+  static const Color primaryColor = Color(0xc503cccc); //main color
+  static const Color secondaryColor = Color(0xFF01678F);  //main dark color or same if not gradient show
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color textColorLight = Color(0xFF12262E);  // text color white used to shown text
+  static const Color textColorDark = Color(0xFF12262E);// text color black
+//background: #12262E;
+  static const Color hintColor = Color(0xFFa1a3ce);
+  static const Color simmer2Color = Color(0xfffafafa);
+  static const Color simmerColor = Color(0xe8c0c1c4);
+  static const Color outline = Color(0xFFa1a3ce);
+  static const Color pinkLytColor = Color(0xFFF0E6FF);
+  static const Color pinkColor = Color(0xE8833066);
+  static const Color orangeColor = Color(0xEDF6935E);
+  static const Color orangeColorS = Color.fromARGB(255, 251, 207, 74);
+  static const Color yelloColor = Color(0xFFEAC42D);
+  static const Color blueLytColor = Color(0xFFEBF2F5);
+  static const Color blueColor = Color(0xFF0214AF);
+  static const Color blueColorlyt = Color(0xFF5163F8);
+  static const Color textColor = Color(0xFF2D2D2D);
+  static const Color themeBackgroundColor = Color(0xFFedf3fa);
+  static const Color themeColor = Color(0xFF34d3fc);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color whiteColorTrans = Color(0xDFFFFFFF);
+  static const Color TransColor = Color.fromARGB(125, 200, 242, 252);
+  static const Color whiteLytColor = Color(0x99D3D3D3);
+  static const Color redColor = Color(0xFFCB1813);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color grayColor = Color(0xFF989898);
+  static const Color lightGrayColor = Color(0xFFa8a8a8);
+  static const Color green = Color(0xff5dff6a);
+
+}
